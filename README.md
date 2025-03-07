96b3eb9290809947d1ead5ccaa9c1da023892538
Subuking001 <3subratmishra1sep@gmail.com>
2025-03-07 19:42:04 +0530

    new

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ## This is the python app
+## try 1
--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,12 +1,11 @@
 from src.math_operation import add, sub
 
-def test_add():
-    assert add(2,3)==5
-    assert add(1,-1)==0
-    assert add(3,2)==10
+def test_addition():
+    assert add(2, 3) == 5
+    assert add(1, -1) == 0
+    assert add(3, 7) == 10  
 
-
-def test_add():
-    assert sub(4,1)==3
-    assert sub(1,1)==0
-    assert sub(3,2)==10
+def test_subtraction():
+    assert sub(4, 1) == 3
+    assert sub(1, 1) == 0
+    assert sub(3, 2) == 1
